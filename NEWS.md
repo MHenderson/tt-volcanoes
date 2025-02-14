4a0877804c5d35943f9f0206cf041dbd06df287b
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-02-14 16:52:36 +0000

    Prepare first release (#17)
    * Remove unused scripts.
    
    * Clean up the main targets pipeline script.
    
    * Re-init renv.
    
    * Update title in README.
    
    * Add a Makefile.
    
    * Re-run the main pipeline.
    
    * Add release notes.

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# tt-volcanoes (development version)
+
+# tt-volcanoes 0.1.0
+
+* Finished on Friday 14th February 2025.
+* Tidy Tuesday for week 20 of 2020.
+* Plot of Volcano Explosivity Index for eruptions around the Pacific Ring of Fire.
+* Data from The Smithsonian Institution (https://volcano.si.edu/).
+* Using ggplot2, ggrepel, rnaturalearth, sf, viridis packages for data visualisation.
+* Using dplyr, glue, readr, tibble packages for data transformation.
--- _targets.R
+++ _targets.R
@@ -1,45 +1,29 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
-
-# Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
 
-# Set target options:
 tar_option_set(
-  packages = c("dplyr", "ggplot2", "ggrepel", "glue", "lubridate", "readr", "rnaturalearth", "sf", "tibble", "viridis"), # packages that your targets need to run
-  format = "rds" # default storage format
-  # Set other options as needed.
+  packages = c("dplyr", "ggplot2", "ggrepel", "glue", "lubridate", "readr", "rnaturalearth", "sf", "tibble", "viridis"),
+  format = "rds"
 )
 
-# tar_make_clustermq() configuration (okay to leave alone):
 options(clustermq.scheduler = "multicore")
 
-# tar_make_future() configuration (okay to leave alone):
-# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
-
-# Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed. # nolint
 
-# Replace the target list below with your own:
 list(
   tar_target(
-    name = eruptions,
+       name = eruptions,
     command = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/eruptions.csv')
   ),
   tar_target(
-    name = volcano,
+       name = volcano,
     command = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
   ),
   tar_target(
-    name = world,
+        name = world,
     command = ne_countries(scale = "medium", returnclass = "sf")
   ),
   tar_target(
-    name = eruptions_sf,
+       name = eruptions_sf,
     command = eruptions |>
       st_as_sf(
         coords = c("longitude", "latitude"),
@@ -49,16 +33,22 @@ list(
       st_transform(crs = st_crs(3832))
   ),
   tar_target(
-    name = eruptions_sf_pp,
+       name = eruptions_sf_pp,
     command = preprocess(eruptions_sf)
   ),
   tar_target(
-    name = eruptions_plot,
+       name = eruptions_plot,
     command = plot_eruptions(eruptions_sf_pp, world)
   ),
   tar_target(
-    name = save_eruptions_plot,
-    command = ggsave(plot = eruptions_plot, filename = "img/eruptions.png", bg = "white", width = 8, height = 8),
-    format = "file"
+       name = save_eruptions_plot,
+    command = ggsave(
+          plot = eruptions_plot,
+      filename = "img/eruptions.png",
+            bg = "white",
+         width = 8,
+        height = 8
+    ),
+     format = "file"
   )
 )
